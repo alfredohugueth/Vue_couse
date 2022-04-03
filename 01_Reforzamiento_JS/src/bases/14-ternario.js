b02af9b6818a988f8111c1fe882aae7cb8f4c464
Alfredo Hugueth Polo <alfredohugueth99@gmail.com>
2022-04-03 15:22:26 -0500

    Conditionals in js

---
+++ 01_Reforzamiento_JS/src/bases/14-ternario.js
@@ -0,0 +1,14 @@
+
+
+let firstName;
+
+let lastName = 'Herrera';
+
+console.log(`${firstName || 'No fistName'} ${ lastName || 'No lastName'}`);
+
+
+const isActive = false
+
+const msg = isActive ? 'activo' : 'Inactivo'
+
+console.log(msg);
