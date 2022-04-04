10b44a373f36519a66daba241102fe733ba510b1
Alfredo Hugueth Polo <alfredohugueth99@gmail.com>
2022-04-04 07:32:42 -0500

    Options Api lesson and using data function in vue

--- 02_bases_vue/app.js
+++ 02_bases_vue/app.js
@@ -1,11 +1,17 @@
 
-
+// Options Api
 const app = Vue.createApp({
     // template: `
     // <h1> Hola mundo </h1>
     // <p> Desde app.js</p>
     // `,
-
+    
+    data() {
+        return {
+            author : 'Bruce Wayne',
+            quote : 'Soy Batman'
+        }
+    }
 })
 
 app.mount('#myApp')
