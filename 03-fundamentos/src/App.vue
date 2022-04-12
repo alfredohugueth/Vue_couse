f1fb020d234db376a710560eedc79ef3a64a9bd0
Alfredo Hugueth Polo <alfredohugueth99@gmail.com>
2022-04-12 16:20:26 -0500

    Correcting files for new ide propose

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Counter title="Salieron" :start='5' />
+<Indecision />
 </template>
 
 <script>
-import Counter from './components/Counter.vue'
+import Indecision from './components/indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    Counter
+    Indecision
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> 
+</style>
---
+++ 03-fundamentos/src/components/indecision.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <!--suppress HtmlUnknownTarget -->
+  <img src="https:via.placeholder.com/250" alt="Indecition">
+    
+    <div class="bg-dark"></div>
+
+    <div class="div indecision-container">
+        
+        <input type="text" placeholder="Hazme una pregunta" v-model="question">
+        <p>
+            Recuerda terminar con un signo de interrogación (?)
+            
+        </p>
+
+        <div>
+            <h2>
+                {{ question }}
+            </h2>
+            <h1>
+                Sí, No, ...Pensando
+            </h1>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            question: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+  img, .bg-dark {
+        height: 100vh;
+        left: 0;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+</style>
