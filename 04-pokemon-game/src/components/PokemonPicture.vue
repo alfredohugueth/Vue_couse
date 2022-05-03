fcd2fe4252652398f028923bdc0a27c042a7dffb
alfredohugueth <alfredohugueth99@gmail.com>
2022-05-03 13:54:27 -0500

    Components of Pokémon application ready for add logic

--- 04-pokemon-game/src/components/PokemonPicture.vue
+++ 04-pokemon-game/src/components/PokemonPicture.vue
@@ -1,6 +1,14 @@
 <template>
   <h1>
-    Pokemon Picture
+    <div class="pokemon-container">
+    <!--  Sin color  -->
+    <img class="hidden-pokemon"
+         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         alt="pokemon">
+
+<!--    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"-->
+<!--         alt="pokemon">-->
+    </div>
   </h1>
 </template>
 
@@ -11,5 +19,22 @@ export default {
 </script>
 
 <style scoped>
-
+/* Pokemon Picture */
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 </style>
